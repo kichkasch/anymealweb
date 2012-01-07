8b9cd0b0347acec6bc07abb536d4072237540779
Michael Pilgermann <kichkasch@gmx.de>
2012-01-07 18:04:48 +0100

    at least adding of recipe is well on the way

--- mealmanager.php
+++ mealmanager.php
@@ -5,15 +5,35 @@
                 <title>JumiMeal Meal Manager</title>
                 <link type="text/css" href="css/sunny/jquery-ui-1.8.16.custom.css" rel="stylesheet" />  
                 <script type="text/javascript" src="scripts/jquery-1.6.2.min.js"></script>
-                <script type="text/javascript" src="scripts/jquery-ui-1.8.16.custom.min.js"></script>                
+                <script type="text/javascript" src="scripts/jquery-ui-1.8.16.custom.min.js"></script> 
+                
+				    <!-- tiny table stuff -->
+				    <link href="css/jquery.ui.tinytbl.css" rel="stylesheet" type="text/css" />
+				    <script src="scripts/jquery.ui.tinytbl.js" type="text/javascript"></script>
+				    <!-- tiny table stuff -->
+				             
+                               
                 <script type="text/javascript">
 
                         $(function(){
 										  $( "#dialogAdd" ).dialog({
 														autoOpen: false,
 														modal: true,
+														width: 400,
 														buttons: {
 															"Add this Recipe": function() {
+																$( this ).dialog( "close" );
+															},
+															Cancel: function() {
+																$( this ).dialog( "close" );
+															}
+														}
+													});                                
+										  $( "#dialogRecipeCategory" ).dialog({
+														autoOpen: false,
+														modal: true,
+														buttons: {
+															"Apply Changes": function() {
 																$( this ).dialog( "close" );
 															},
 															Cancel: function() {
@@ -24,13 +44,30 @@
                                 $( "#accordion" ).accordion( {
                                 		collapsible:true
                                 		});
-                                $( "#radio" ).buttonset();
-                                $( "#bAdd" ).button();
+                                $( "#radio" ).buttonset(); // Obere Liste mit den Kategorien
+                                $( "#bAdd" ).button();     // Ein Rezept hinzufügen
+                                $( "#ingredButtonSet" ).buttonset(); // im Dialog Rezept hinzufügen - Aktionen für Zutaten
+                                
                                 
                                 $( "#bAdd" ).click(function() {
 												$( "#dialogAdd" ).dialog( "open" );
+											       convertTable($("#ingredients"));
+												
 												return false;
 											});
+											
+											$( "#bAddIngredient" ).click(function() {
+												$("#ingredients").tinytbl('append', $('<tr><td>200</td><td>Gramm</td><td>Mehl</td></tr>'));
+												return false;
+												});
+											$( "#bClearIngredientList" ).click(function() {
+												recoverTable($("#ingredients"));
+												$("#ingredients tbody").empty();
+												convertTable($("#ingredients"));
+												return false;
+												});
+
+
 								});
 								
 								
@@ -39,7 +76,23 @@
 												$(this).next().toggle('slow');
 												return false;
 											}).next().hide();
+											
 								} );
+								
+								function convertTable(theTable) {
+									theTable.tinytbl({
+							            direction: 'ltr',      // text-direction (default: 'ltr')
+							            thead:     true,       // fixed table thead
+							            tfoot:     false,       // fixed table tfoot
+							            cols:      '0',          // fixed number of columns
+							            width:     '100%',     // table width (default: 'auto')
+							            height:    '100px'      // table height (default: 'auto')
+							        });									
+							       };
+									
+ 								function recoverTable(theTable) {
+ 									theTable.tinytbl('destroy');
+									};		 				
 								
 								function catSelected(category) {
 										$( "#accordion" ).accordion('activate', false);
@@ -58,8 +111,12 @@
 												}); 	
 											}											
     									});
-    									//alert(category + st );
 									};
+									
+								function editCategoryAssociation(recipeId) {
+									$( "#dialogRecipeCat_recipeName" ).text(recipeId);
+									$( "#dialogRecipeCategory" ).dialog( "open" );
+									}
 
                 </script>
                 <style type="text/css">
@@ -85,18 +142,44 @@ include 'config.php';
 
 <button id="bAdd">Add new Recipe</button>
 <div id="dialogAdd" title="Add new Recipe">
-	<p>This is an animated dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the 'x' icon.</p>
+	<p>Please provide details for your recipe.</p>
 
 	<form>
 	<fieldset>
-		<p><label for="name">Name</label><br/>
-		<input type="text" name="name" id="name" class="text ui-widget-content ui-corner-all" /></p>
+		<p><label for="name">Title of Recipe</label><br/>
+		<input type="text" name="name" id="name" size="50" class="text ui-widget-content ui-corner-all" /></p>
 		
-		<p><label for="email">Email</label><br/>
-		<input type="text" name="email" id="email" value="" class="text ui-widget-content ui-corner-all" /></p>
+		<p><label for="ingredients">Ingredients</label><br/>
 		
-		<p><label for="password">Password</label><br/>
-		<input type="password" name="password" id="password" value="" class="text ui-widget-content ui-corner-all" /></p>
+<table class="ui-tinytable" name="ingredients" id="ingredients">
+    <thead>
+        <tr>
+            <th>&nbsp;&nbsp;&nbsp;Amount&nbsp;&nbsp;&nbsp;</th>
+            <th>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Unit&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</th>
+            <th>&nbsp;&nbsp;Ingredient&nbsp;&nbsp;</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>20</td>
+            <td>Gramm</td>
+            <td>Zucker</td>
+        </tr>
+        <tr>
+            <td>500</td>
+            <td>Gramm</td>
+            <td>Butter</td>
+        </tr>
+    </tbody>
+</table>
+		</p>
+<div id="ingredButtonSet">
+<button id="bAddIngredient">Add an ingredient</button>
+<button id="bClearIngredientList">Clear list</button>
+</div>
+		
+		<p><label for="preparation">Preparation</label><br/>
+		<textarea name="preparation" id="preparation" value="" cols="50" rows="8" class="text ui-widget-content ui-corner-all" ></textarea></p>
 	</fieldset>
 	</form>	
 </div>
@@ -147,7 +230,9 @@ for($x = 0 ; $x < mysql_num_rows($resultID) ; $x++){
  <thead>
  <th>Ingredients</th>
  <th>Preparation</th>
- <th width="10%">Categories</th> 
+ <?php
+ print('<th width="10%">Categories <a onclick="editCategoryAssociation(' . $row['ID'] . ')"><img src="images/edit.png" width="20" height="20" alt="Edit Categories for this Recipe"></a></th>'); 
+ ?> 
  </thead>
  <tr>
  <td>n.a.</td>
@@ -167,6 +252,21 @@ unset($cats);
 <?php
 }
 ?>
+
+<div id="dialogRecipeCategory" title="Assign Categories to Recipe">
+	<p>Change categories under which recipe <div id="dialogRecipeCat_recipeName"></div> shall be filed.</p>
+
+<?php
+ $query2 = "SELECT CATEGORIES.NAME as NAME FROM CATEGORIES ORDER BY NAME";
+ $resultID2 = mysql_query($query2, $linkID) or die("Data not found.");
+ for($y = 0 ; $y < mysql_num_rows($resultID2) ; $y++){
+   $row2 = mysql_fetch_assoc($resultID2);
+ 	print('<input type="checkbox" id="check_" /><label for="check1">' . $row2['NAME'] .  '</label><br/>');
+}
+?>
+
+</div>
+
 
 </div>
 
